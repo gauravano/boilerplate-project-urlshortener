965f0e2a975ad39f412cc4d11ef0c685ac9f7a6c
gauravano <sachdeva.gaurav1997@gmail.com>
2018-11-19 04:05:04 +0530

    envir. variables set and db created

--- server.js
+++ server.js
@@ -3,20 +3,20 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
 var cors = require('cors');
-
+var bodyParser = require('body-parser');
 var app = express();
 
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGO_URI);
 
 app.use(cors());
 
-/** this project needs to parse POST bodies **/
+app.use(bodyParser.urlencoded({extended: 'false'}));
+app.use(bodyParser.json());
+
 // you should mount the body-parser here
 
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -25,11 +25,11 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+
 
 
 app.listen(port, function () {
